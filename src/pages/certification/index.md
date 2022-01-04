f0e9363837763f19921d1796b838dd0f79d117f0
ichizokichinosuke <nuuuukemw@gmail.com>
2022-01-05 02:23:30 +0900

    add screenshots

--- src/pages/certification/index.md
+++ src/pages/certification/index.md
@@ -4,8 +4,21 @@ path: /cert
 image: /img/air.jpg
 title: お取り引き先の方へ
 subheading: To customers
+syobunkyokaImage: /img/県収集運搬.png
 
 ---
 ## 許可証
+
+---
+
+### 産業廃棄物処分業許可
+
+---
+
+### 産業廃棄物収集運搬業許可
+
+---
+
+### かごしま認定リサイクル製品認定証
 
 ---
--- src/templates/cert-page.js
+++ src/templates/cert-page.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Proptypes from "prop-types";
 import { graphql } from "gatsby";
-import { getImage } from "gatsby-plugin-image";
+import { getImage, StaticImage } from "gatsby-plugin-image";
 
 import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
 import FullWidthImage from "../components/FullWidthImage";
+// import syobunImage from "../img/処分許可証.png";
 
 
 export const CertPageTemplate = ({
@@ -13,10 +14,16 @@ export const CertPageTemplate = ({
     title,
     subheading,
     content,
-    contentComponent
+    contentComponent,
+    syobunkyokaImage,
 }) => {
     const heroImage = getImage(image) || image;
+    // const syobunImage = getImage(syobunkyokaImage) || syobunkyokaImage;
     const PageContent = contentComponent || Content;
+    const syobunImage = "../img/処分許可証.png";
+    const syuunImage = "../img/県収集運搬.png";
+    const imageHeight = 250;
+    // const imageWidth = "2rem";
 
     return (
         <div>
@@ -26,13 +33,53 @@ export const CertPageTemplate = ({
                     <div className="columns">
                         <div className="column is-10 is-offset-1">
                             <div className="section">
-                                <PageContent className="content" content={content} />
+                                <h1 className="title is-2">許可証</h1>
+                                {/* <div className="block"></div> */}
+                                <h2 className="title is-3">産業廃棄物処分業許可</h2>
+                                <div className="columns">
+                                    <div className="column">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                    </div>
+                                    <div className="column is-4">
+                                        {/* test */}
+                                        {/* {syobunkyokaImageVar} */}
+                                        {/* <FullWidthImage img={syobunkyokaImageVar} /> */}
+                                        <StaticImage src={syobunImage} height={imageHeight}/>
+                                        {/* <img src={syobunImage} /> */}
+                                    </div>
+                                </div>
+                                <h2 className="title is-3">産業廃棄物収集運搬業許可</h2>
+                                <div className="columns">
+                                    <div className="column">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                    </div>
+                                    <div className="column is-4">
+                                        {/* test */}
+                                        {/* {syobunkyokaImageVar} */}
+                                        {/* <FullWidthImage img={syobunkyokaImageVar} /> */}
+                                        <StaticImage src={syuunImage} height={imageHeight}/>
+                                        {/* <img src={syobunImage} /> */}
+                                    </div>
+                                </div>
+                                <h2 className="title is-3">かごしま認定リサイクル製品認定証</h2>
+                                <div className="columns">
+                                    <div className="column">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                    </div>
+                                    <div className="column is-4">
+                                        {/* test */}
+                                        {/* {syobunkyokaImageVar} */}
+                                        {/* <FullWidthImage img={syobunkyokaImageVar} /> */}
+                                        <StaticImage src={syuunImage} height={imageHeight}/>
+                                        {/* <img src={syobunImage} /> */}
+                                    </div>
+                                </div>
+                                {/* <PageContent className="content" content={content} /> */}
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
-
         </div>
     )
 }
@@ -47,6 +94,7 @@ const CertPage = ({ data }) => {
                 title={post.frontmatter.title}
                 subheading={post.frontmatter.subheading}
                 content={post.html}
+                syobunkyokaImage={post.frontmatter.syobunkyokaImage}
             />
         </Layout>
     )
