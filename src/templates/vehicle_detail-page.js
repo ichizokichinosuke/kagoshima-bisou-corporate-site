2bfccd718e79f13f1b36c1e579632519b44285e2
ichizokichinosuke <nuuuukemw@gmail.com>
2022-02-09 02:12:54 +0900

    add table

--- src/templates/vehicle_detail-page.js
+++ src/templates/vehicle_detail-page.js
@@ -6,6 +6,8 @@ import { graphql } from "gatsby";
 
 export const VehicleDetailTemplate = ({ image, title, subheading }) => {
   const heroImage = getImage(image) || image;
+  const hino10tDump = "../img/hino-10tdump.jpg";
+  const imageHeight = 300;
   return (
     <div>
       <FullWidthImage img={heroImage} title={title} subheading={subheading} />
@@ -14,118 +16,50 @@ export const VehicleDetailTemplate = ({ image, title, subheading }) => {
           <div className="columns">
             <div className="column is-10 is-offset-1">
               <div className="section">
-                <h1 className="title is-2">破砕施設</h1>
-                <div className="content">
-                  <table className="table is-hoverable is-bordered has-text-lefted">
-                    <thead>
-                      <tr>
-                        <th>処理可能ごみ</th>
-                        <th>処理能力</th>
-                      </tr>
-                    </thead>
 
-                    <tbody>
-                      <tr>
-                        <th>1. 木くず</th>
-                        <td>3.5t/日</td>
-                      </tr>
-                      <tr>
-                        <th>2. 廃プラスチック類</th>
-                        <td>0.79t/日</td>
-                      </tr>
-                      <tr>
-                        <th>3. ガラス、コンクリート、陶磁器、紙くず</th>
-                        <td>4.8t/日</td>
-                      </tr>
-                      <tr>
-                        <th>4. がれき類、ガラス、コンクリート、陶磁器くず</th>
-                        <td>288t/日</td>
-                      </tr>
-                      <tr>
-                          <th>5. 金属（蛍光灯等）、ガラス、コンクリート、陶磁器くず</th>
-                          <td>3.1t/日</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-                <h1 className="title is-2">圧縮施設</h1>
                 <div className="content">
-                    <table className="table is-hoverable is-bordered has-text-lefted">
-                        <thead>
-                            <tr>
-                                <th>処理可能ごみ</th>
-                                <th>処理能力</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th>廃プラスチック類</th>
-                                <td>13.2t/日</td>
-                            </tr>
-                            <tr>
-                                <th>ゴムくず</th>
-                                <td>19.7t/日</td>
-                            </tr>
-                            <tr>
-                                <th>金属くず</th>
-                                <td>42.7t/日</td>
-                            </tr>
-                            <tr>
-                                <th>紙くず</th>
-                                <td>11.3t/日</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <h1 className="title is-2">選別施設</h1>
-                <div className="content">
-                    <table className="table is-hoverable is-bordered has-text-lefted">
-                        <thead>
-                            <tr>
-                                <th>処理可能ごみ</th>
-                                <th>処理能力</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th>ガラス、コンクリート、陶磁器、がれき類、金属、ゴムくず</th>
-                                <td>222t/日</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <h1 className="title is-2">焼却施設</h1>
-                <div className="content">
-                    <table className="table is-hoverable is-bordered has-text-lefted">
-                        <thead>
-                            <tr>
-                                <th>処理可能ごみ</th>
-                                <th>処理能力</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th>紙くず</th>
-                                <td>236.80kg/日</td>
-                            </tr>
-                            <tr>
-                                <th>木くず</th>
-                                <td>222.96kg/日</td>
-                            </tr>
-                            <tr>
-                                <th>繊維くず</th>
-                                <td>197.28kg/日</td>
-                            </tr>
-                            <tr>
-                                <th>ゴムくず</th>
-                                <td>89.84kg/日</td>
-                            </tr>
-                            <tr>
-                                <th>廃プラスチック類</th>
-                                <td>89.20kg/日</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <h3 className="title is-3">日野 10tダンプ</h3>
+                    <div className="columns">
+                        <div className="column">
+                            <StaticImage src={hino10tDump} height={imageHeight}/>
+                        </div>
+                        <div className="column">
+                            <table className="table is-hoverable has-text-lefted is-striped">
+                                <tbody>
+                                    <tr>
+                                        <th>乗車定員</th>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <th>最大積載量</th>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <th>車両総重量</th>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <th>長さ</th>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <th>幅</th>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <th>高さ</th>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <th>総排気量</th>
+                                        <td></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+
                 </div>
               </div>
             </div>
