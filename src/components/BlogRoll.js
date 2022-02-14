b2da9918caa574555eb0aaa4af95de93b5866287
ichizokichinosuke <nuuuukemw@gmail.com>
2022-02-15 01:45:14 +0900

    apply japanese

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -84,7 +84,7 @@ export default function BlogRoll() {
           ) {
             edges {
               node {
-                excerpt(pruneLength: 400)
+                excerpt(pruneLength: 30, truncate: true)
                 id
                 fields {
                   slug
