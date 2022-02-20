63a05370d7ed9faba5894626886a59e2a801442e
ichizokichinosuke <nuuuukemw@gmail.com>
2022-02-20 19:09:27 +0900

    update recruit

--- src/templates/recruit-page.js
+++ src/templates/recruit-page.js
@@ -27,11 +27,11 @@ const RecruitPageTemplate = ({
               <div className="section">
                 <div className="content">
                     <blockquote>
-                        <h4>事業拡大のため、共に働く仲間を随時募集しています!</h4>
+                        <h4>事業拡張のため、共に働く仲間を随時募集しています!</h4>
                         <h4>持続可能な社会づくりに一緒に取り組みましょう!</h4>
                     </blockquote>
                 </div>
-                <h2 className="title is-2">解体土木作業員、一般事務</h2>
+                <h2 className="title is-2">解体・土木作業員、一般事務</h2>
                 <div className="columns">
                   <div className="column">
                     <div className="content">
@@ -40,7 +40,7 @@ const RecruitPageTemplate = ({
                           <tr>
                             <th>募集職種</th>
                             <td>
-                              解体土木作業員
+                              解体・土木作業員
                               <br />一般事務
                             </td>
                           </tr>
@@ -74,13 +74,13 @@ const RecruitPageTemplate = ({
                           </tr>
                           <tr>
                             <th>福利厚生</th>
-                            <td>各種保険(雇用、労災、健康、厚生)</td>
+                            <td>制服貸与、各種保険(雇用、労災、健康、厚生)</td>
                           </tr>
                           <tr>
                             <th>応募資格</th>
                             <td>学歴不問／業種・職種未経験者、第二新卒歓迎<br />
-                                ・Word、Excel、Powerpointの基本的な操作スキルをお持ちの方<br />
-                                ※建設・産廃・解体土木業界の知識があれば経験を活かせます。<br />
+                                ・Word、Excel、Powerpoint等のOfficeソフトの基本的な操作スキルをお持ちの方<br />
+                                ※建設・産廃・解体・土木業界の知識があれば経験を活かせます。<br />
                                 ※大型免許、土木1級2級保持者、車両系重機、免許保持者は当社規定により優遇します。<br />
                                 ※女性の活躍を積極的に支援するため、ポジティブアクションを適用しています。
                             </td>
